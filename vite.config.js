52d8741b1bfc498fc9fe1913c68393d336c3d6a7
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-11-16 11:46:51 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import { ghPages } from "vite-plugin-gh-pages";
+// import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/product-list-with-cart/",
-  plugins: [react(), eslint(), ghPages()],
+  plugins: [react(), eslint()],
 });
